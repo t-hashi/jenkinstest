d903a2c0ab3ff03d7d392557470026853d8e970c
t-hashi <g.toru.hashimoto@gmail.com>
2022-06-28 19:41:55 +0900

    commit test.

---
+++ src/sample/Main.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World.");
+		System.out.println(test("This is test."));
+	}
+
+	private static String test(String input) {
+		return input;
+	}
+}
